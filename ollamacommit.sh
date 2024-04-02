9c11d05f5c2e9e771e531f84d07cd0f9870cfbeb
David Aerne <meodai@gmail.com>
2024-04-02 12:45:33 +0200

    Update script files: ollamacommit.sh and tv.sh - Modified ollamacommit.sh to generate a commit message using Ollama. - Added error handling in tv.sh for failed playlist fetching. - Improved error messages for better user experience.

--- ollamacommit.sh
+++ ollamacommit.sh
@@ -16,15 +16,27 @@ fi
 
 STATUS=$(git status -v)
 
-OLLAMAMSG="
-Imagine being a developer, you just wrote some code and you're ready to commit it. 
-You run 'git status -v' and see the following changes. Summerize the changes as a commit message that could be used to commit these changes.
+ollamacommitmsg() {
+  OLLAMAMSG="
+  Imagine being a developer, you just wrote some code and you're ready to commit 
+  it. You run 'git status -v' and see the following changes. Summerize the 
+  changes as a commit message that could be used to commit these changes. 
+  Imperative writing.
 
-$STATUS
-"
+  $STATUS
+  "
 
-# feed the changes to ollama using git status -v
-echo "$OLLAMAMSG" | ollama run $OLLAMAMODEL
+  # feed the changes to ollama using git status -v
+  OLLAMAMESSAGE=$(echo $OLLAMAMSG | ollama run $OLLAMAMODEL)
+
+  # remove eventual "Commit message:" string from the OLLAMAMESSAGE
+  OLLAMAMESSAGE=$(echo $OLLAMAMESSAGE | sed 's/Commit message://g')
+
+  # echo the OLLAMAMESSAGE
+  echo $OLLAMAMESSAGE
+}
+
+ollamacommitmsg
 
 # Exit the script with a success status
 exit 0
--- tv.sh
+++ tv.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+tv7 () {
 echo "Fetching new playlist"
 TVCURL=$(curl --fail https://api.init7.net/tvchannels.xspf -o ~/Movies/Fiber7.TV.tmp.xspf)
 if [[ $TVCURL ]]; then
@@ -11,7 +12,14 @@ if [[ $TVCURL ]]; then
     mv ~/Movies/Fiber7.TV.tmp.xspf ~/Movies/Fiber7.TV.xspf
   fi
 else
-  echo "Fetching new playlist failed, using cached one"
+  echo "
+    Fetching new playlist failed, using cached one,
+    if it does not work it is likely that you are not on a init7 connection,
+    consider VPN'ing home or using a different connection.
+  "
 fi
 open -a vlc ~/Movies/Fiber7.TV.xspf
 #/Applications/VLC.app/Contents/MacOS/VLC -I rc ~/Movies/Fiber7.TV.xspf
+}
+
+tv7
